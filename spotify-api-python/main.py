9235cc313876555d5536b4f4f9e58876fe73e250
Cooper Rachow <cooper.rachow@icloud.com>
2024-05-19 17:43:52 -0500

    update

--- spotify-api-python/main.py
+++ spotify-api-python/main.py
@@ -12,63 +12,11 @@ CLIENT_ID="05df09b03e7d4f82b868e3a3ac4d769c"
 CLIENT_SECRET="344f2abfa80f4b67955feff1bc0fe659"
 
 # Set up Spotipy client with user authorization
-scope = "user-top-read"  # Scope for reading user's top artists
+scope = "user-top-read playlist-modify-public playlist-modify-private"
 sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
                         client_secret=CLIENT_SECRET,
                         redirect_uri="http://localhost:5000/callback",
                         scope=scope)
-
-# when we go to base route, we run ths index function which authorizes
-@app.route('/')
-def index():
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-@app.route('/callback')
-def callback():
-    # retrieve user input (neglecting spaces right now)
-    artist_names = []
-    num = int(input("How many artists would you like to add? (1 or more): "))
-    print("When using spaces, surround entire name with quotes!!!")
-    for i in range(0, num):
-        artist = input("Enter Name: ")
-        artist_names.append(artist)
-
-    access_token = get_access_token()
-    sp = spotipy.Spotify(auth=access_token)
-
-    # get artist id's 
-    artist_ids = []
-    for name in artist_names:
-        artist_id = get_artist_id(sp, name)
-        artist_ids.append(artist_id)
-
-    # print(artist_ids)
-
-    # get each artists' albums
-    albums = get_artists_albums(sp, artist_ids)
-    # display proper key value pair to user excluding ID. ie.; 1: album name
-    for album in albums:
-        for key, value in album.items():
-            album_num = key
-            album_name = value[0]
-            print(f"{album_num}: {album_name}")
-    # print(albums)
-
-    # get user album numbers in an array
-    album_nums = input("Enter the numbers of each corresponding album (spaces in between): ")
-    final_nums = album_nums.split()
-
-    album_id_lis = get_user_albums(final_nums, albums)
-    # print(album_id_lis)
-
-    all_tracks = get_album_track_ids(sp, album_id_lis)
-    print(all_tracks)
-
-    
-    # reduce album list 
-
-    return "Authenticated successfully!"
 
 def get_access_token() -> str:
     # grab token dictionary
@@ -80,6 +28,16 @@ def get_access_token() -> str:
     else:
         access_token = token_info['access_token']
     return access_token
+
+def get_color(colors: list[str]) -> str:
+    while True:
+        print("Red | Yellow | Green | Blue | Purple | Orange | Black")
+        color = input("Please enter a color: ").strip().lower()
+        if color in colors:
+            return color
+        else:
+            print("Invalid color. Please enter a valid color.")
+
 
 # takes in an artist name and returns the ID of the artist
 def get_artist_id(sp: object, name: str) -> str:
@@ -139,6 +97,137 @@ def get_album_track_ids(sp: object, IDarr: list[str]) -> list[str]:
             cur = track['id']
             all_tracks.append(cur)
     return all_tracks
+
+def get_track_values(sp: object, IDarr: list[str]) -> list[dict]:
+    tracks_values = []
+    for track in IDarr:
+    # track = IDarr[0]
+        track_values = sp.audio_features(track)
+        # d = track_values[0]['danceability']
+        e = track_values[0]['energy']
+        v = track_values[0]['valence']
+        t = track_values[0]['tempo']
+        element = {track: ({"e":e}, {"v":v}, {"t":t})}
+        tracks_values.append(element)
+    return tracks_values
+
+# matches with correct color and then checks if song succeeds the values of that color
+def color_test (color: str, e:float, v:float, t:float) -> bool:
+    match color:
+        case "blue":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+        case "red":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+        case "green":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+        case "orange":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+        case "yellow":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+        case "black":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+        case "purple":
+            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+                return True
+            else:
+                return False
+
+# when we go to base route, we run ths index function which authorizes
+@app.route('/')
+def index():
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback(): 
+    colors = ["red", "blue","yellow","green","orange","purple","black"]
+    color = get_color(colors)
+
+    # retrieve user input (neglecting spaces right now)
+    artist_names = []
+    num = int(input("How many artists would you like to add? (1 or more): "))
+    print("When using spaces, surround entire name with quotes!!!")
+    for i in range(0, num):
+        artist = input("Enter Name: ")
+        artist_names.append(artist)
+
+    # grab access token
+    access_token = get_access_token()
+    sp = spotipy.Spotify(auth=access_token)
+
+    # get artist id's 
+    artist_ids = []
+    for name in artist_names:
+        artist_id = get_artist_id(sp, name)
+        artist_ids.append(artist_id)
+
+    # print(artist_ids)
+
+    # get each artists' albums
+    albums = get_artists_albums(sp, artist_ids)
+    # display proper key value pair to user excluding ID. ie.; 1: album name
+    for album in albums:
+        for key, value in album.items():
+            album_num = key
+            album_name = value[0]
+            print(f"{album_num}: {album_name}")
+    # print(albums)
+
+    # get user album numbers in an array
+    album_nums = input("Enter the numbers of each corresponding album (spaces in between): ")
+    final_nums = album_nums.split()
+
+
+    album_id_lis = get_user_albums(final_nums, albums)
+
+    all_tracks = get_album_track_ids(sp, album_id_lis)
+
+    all_tracks_values = get_track_values(sp, all_tracks)
+
+    # gets final tracks for playlist
+    passing_tracks = []
+    for track in all_tracks_values:
+        # grab desired values for the track and pass thru test function
+        for tID, tVal in track.items():
+            # d = tVal[0]['d']
+            e = tVal[1]['e']
+            v = tVal[2]['v']
+            t = tVal[3]['t']
+            # print(f"{d}, {e}, {v}, {t}")
+            result = color_test(color, e, v, t)
+            # if track succeeds then append to list
+            if (result):
+                passing_tracks.append(tID)
+
+    print(passing_tracks)
+
+    # user_id = sp.me()['id']
+    # sp.user_playlist_create(user=user_id, name="testing", public=True,description="testing")
+
+
+
+
+    return "Authenticated successfully!"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
