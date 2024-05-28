0e701a956e35ee21c2d489b1d3754efdfa364aa0
Cooper Rachow <cooper.rachow@icloud.com>
2024-05-28 17:44:56 -0500

    cmd fully functional

--- spotify-api-python/main.py
+++ spotify-api-python/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from fuzzywuzzy import fuzz
+import base64
 
 app = Flask(__name__)
 
@@ -12,11 +14,13 @@ CLIENT_ID="05df09b03e7d4f82b868e3a3ac4d769c"
 CLIENT_SECRET="344f2abfa80f4b67955feff1bc0fe659"
 
 # Set up Spotipy client with user authorization
-scope = "user-top-read playlist-modify-public playlist-modify-private"
+scope = "user-top-read playlist-modify-public playlist-modify-private ugc-image-upload"
 sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
                         client_secret=CLIENT_SECRET,
                         redirect_uri="http://localhost:5000/callback",
                         scope=scope)
+
+# redirect_uri="http://192.168.1.128:5000/callback",
 
 def get_access_token() -> str:
     # grab token dictionary
@@ -38,19 +42,34 @@ def get_color(colors: list[str]) -> str:
         else:
             print("Invalid color. Please enter a valid color.")
 
+# requests artists names from user based on number of artists. passes artists through info function for name + ID
+def artists_confirm (sp: object, num: int) -> list[dict]:
+    artist_names = []
+    
+    for i in range(0, num):
+        artist = input("Enter Name: ")
+        artist_names.append(artist)
+        # final names of artists
+    artist_ids = []
+
+    # pass each name into info function to get id too
+    for name in artist_names:
+        artist_id = get_artist_info(sp, name)
+        artist_ids.append(artist_id)
+
+    return artist_ids
 
 # takes in an artist name and returns the ID of the artist
-def get_artist_id(sp: object, name: str) -> str:
-    artist_dict = sp.search(q=name, type='artist', limit=2)
-    # more accurate when i retrieve the top 2 of the search, for some reason when 
-    # limit to 1 it gives wrong person
-    two_artists = artist_dict['artists']['items']
-    # print(two_artists)
-    main_art = two_artists[0]
-    # print(main_art)
-    artist_id = main_art['id']
-    
-    return artist_id
+def get_artist_info(sp: object, name: str) -> dict:
+    artist_dict = sp.search(q=name, type='artist', limit=10)
+
+    artists = artist_dict['artists']['items']
+
+    final = artists[0]
+    artist_id = final['id']
+    artist_name = final['name']
+
+    return {artist_id: artist_name}
 
 # takes in a list of ID's and returns an array containing a number 
 # and corresponsing albums for user
@@ -112,44 +131,68 @@ def get_track_values(sp: object, IDarr: list[str]) -> list[dict]:
     return tracks_values
 
 # matches with correct color and then checks if song succeeds the values of that color
-def color_test (color: str, e:float, v:float, t:float) -> bool:
+def color_test (color: str, e:float, v:float) -> bool:
     match color:
         case "blue":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (0.2 < v < 0.6 and e < 5):
                 return True
             else:
                 return False
         case "red":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (e > 0.7):
                 return True
             else:
                 return False
         case "green":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (0.3 < e < 0.7 and 0.4 < v > 0.7):
                 return True
             else:
                 return False
         case "orange":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (e > 0.65 and v > 0.6):
                 return True
             else:
                 return False
         case "yellow":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (0.3 < e < 0.7 and v > 0.7):
                 return True
             else:
                 return False
         case "black":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (v < 0.4):
                 return True
             else:
                 return False
         case "purple":
-            if (0 < e < 1 and 0 < v < 1 and 0 < t < 1):
+            if (0.3 < v < 0.7 and e < 0.7):
                 return True
             else:
                 return False
 
+
+def create_playlist(sp: object, nameArr: list[str], idArr: list[str], color: str):
+    acr = ""
+    desc = ""
+    image_path = f"covers/{color}.jpg"
+
+    for name in nameArr:
+        acr += name[0]
+        desc += f"{name} "
+
+    title = f"{color.upper()}: {acr}"
+
+    user_id = sp.me()['id']
+    playlist = sp.user_playlist_create(user=user_id, name=title, public=True, description=desc)
+    pID = playlist['id']
+
+    with open(image_path, "rb") as image_file:
+        encoded = base64.b64encode(image_file.read()).decode('utf-8')
+    
+    sp.playlist_upload_cover_image(pID, encoded)
+
+    sp.user_playlist_add_tracks(user_id, pID, idArr)
+
+    return 
 # when we go to base route, we run ths index function which authorizes
 @app.route('/')
 def index():
@@ -158,28 +201,37 @@ def index():
 
 @app.route('/callback')
 def callback(): 
+
+    #  grab access token
+    access_token = get_access_token()
+    sp = spotipy.Spotify(auth=access_token)
+
+
     colors = ["red", "blue","yellow","green","orange","purple","black"]
     color = get_color(colors)
 
     # retrieve user input (neglecting spaces right now)
-    artist_names = []
     num = int(input("How many artists would you like to add? (1 or more): "))
     print("When using spaces, surround entire name with quotes!!!")
-    for i in range(0, num):
-        artist = input("Enter Name: ")
-        artist_names.append(artist)
+    print("The more specific the better the more accurate the results.")
 
-    # grab access token
-    access_token = get_access_token()
-    sp = spotipy.Spotify(auth=access_token)
-
-    # get artist id's 
-    artist_ids = []
-    for name in artist_names:
-        artist_id = get_artist_id(sp, name)
-        artist_ids.append(artist_id)
-
-    # print(artist_ids)
+    stop = True
+    while (stop):
+        artist_ids = []
+        artist_names = []
+        # EX: [{ID: NAME}, {ID2: NAME}, ...]
+        artist_info = artists_confirm(sp, num)
+        for info in artist_info:
+            for key, value in info.items():
+                print(value)
+                artist_names.append(value)
+                artist_ids.append(key)
+        # print(artist_info)
+        confirmation = input("are these the correct artist (y/n)? ").lower()
+        if confirmation == 'y' or confirmation == 'yes':
+            stop = False
+        if stop == True:
+            print("Please re-enter your artist more specifically.")
 
     # get each artists' albums
     albums = get_artists_albums(sp, artist_ids)
@@ -207,23 +259,15 @@ def callback():
     for track in all_tracks_values:
         # grab desired values for the track and pass thru test function
         for tID, tVal in track.items():
-            # d = tVal[0]['d']
-            e = tVal[1]['e']
-            v = tVal[2]['v']
-            t = tVal[3]['t']
+            e = tVal[0]['e']
+            v = tVal[1]['v']
             # print(f"{d}, {e}, {v}, {t}")
-            result = color_test(color, e, v, t)
+            result = color_test(color, e, v)
             # if track succeeds then append to list
             if (result):
                 passing_tracks.append(tID)
 
-    print(passing_tracks)
-
-    # user_id = sp.me()['id']
-    # sp.user_playlist_create(user=user_id, name="testing", public=True,description="testing")
-
-
-
+    create_playlist(sp, artist_names, passing_tracks, color)
 
     return "Authenticated successfully!"
 
@@ -231,4 +275,5 @@ def callback():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # app.run(host='192.168.1.128', port=5000)
 
--- spotify-api-python/testing.py
+++ spotify-api-python/testing.py
@@ -1,47 +1,18 @@
-from flask import Flask, request, redirect
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import os
 
-app = Flask(__name__)
+# Get the directory path of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Load environment variables
-from dotenv import load_dotenv
-load_dotenv()
+# Define the path to the images folder
+images_folder = os.path.join(script_dir, 'covers')
 
-CLIENT_ID="05df09b03e7d4f82b868e3a3ac4d769c"
-CLIENT_SECRET="344f2abfa80f4b67955feff1bc0fe659"
+# List all files in the images folder
+image_files = os.listdir(images_folder)
 
-# Set up Spotipy client with user authorization
-scope = "user-top-read"  # Scope for reading user's top artists
-sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
-                        client_secret=CLIENT_SECRET,
-                        redirect_uri="http://localhost:5000/callback",
-                        scope=scope)
-
-@app.route('/')
-def index():
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    access_token = token_info['access_token']
-
-    sp = spotipy.Spotify(auth=access_token)
-    
-    top_artists = get_top_artists(sp)
-    if top_artists:
-        print("Your top 5 most listened to artists:")
-        for index, artist in enumerate(top_artists):
-            print(f"{index+1}. {artist['name']}")
-    
-    return "Authenticated successfully!"
-
-def get_top_artists(sp, limit=5):
-    result = sp.current_user_top_artists(limit=limit, time_range='long_term')
-    return result['items']
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Iterate over the image files and do something with them
+for image_file in image_files:
+    if image_file.endswith('.jpg'):
+        # Construct the full path to each image file
+        image_path = os.path.join(images_folder, image_file)
+        # Now you can use this image_path to access each image file
+        print(image_path)
